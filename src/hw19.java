01c6ccc8ac92956b56539b7da17e2bf08c6b4ee6
Shabadam <shabadamalsu@gmail.com>
2023-04-20 16:14:09 +0500

    Changed

--- src/hw19.java
+++ src/hw19.java
@@ -1,13 +1,13 @@
 public class hw19 {
     public static void main(String[] args) {
-        int s = 100;
-        int p = 1100;
-        int x = 0;
-        if (p > 1000) {
-            x = p / 100;
+        int check = 100;
+        int plus = 1100;
+        int bonus = 0;
+        if (plus > 1000) {
+            bonus = plus / 100;
         }
-        s = s + p + x;
-        System.out.println(s);
-        System.out.println(x);
+        check = check + plus + bonus;
+        System.out.println(check);
+        System.out.println(bonus);
     }
 }
